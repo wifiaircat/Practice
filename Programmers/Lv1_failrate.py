6aa7d73ce4bda0be285fdf392d930b373e58368f
wifiaircat <cocoumum0000@gmail.com>
2024-03-18 15:21:32 +0900

    실행시간을 줄이기 위해서 len과 count 함수를 최대한 반복되지 않게 반복문 바깥으로. 그리고 remove도 사용하지 않음

--- Programmers/Lv1_failrate.py
+++ Programmers/Lv1_failrate.py
@@ -5,13 +5,13 @@ N = 5
 stages = [2, 1, 2, 6, 2, 4, 3, 3]
 
 fail = {} #dict
+giri = len(stages) # 8
 for i in range(1, N+1): # 1 2 3 4 5
-    if i not in stages:
+    cnt = stages.count(i)
+    if cnt == 0:
         fail[i] = 0
     else:
-        fail[i] = stages.count(i) / len(stages)
-        
-        while i in stages:
-            stages.remove(i)
+        fail[i] = cnt/ giri
+        giri -= cnt
 ans = sorted(fail.items(), key = lambda x: x[1], reverse = True)
 print([ans[i][0] for i in range(N)])
