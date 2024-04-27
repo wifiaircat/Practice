3234494f2d887dd0154a1cd96c58387988d588e4
wifiaircat <cocoumum0000@gmail.com>
2024-04-27 19:08:57 +0900

    우선순위 위치 리스트를 만들어서 들어있으면 먼저 보내기

--- etc/OthelloPlayer2.py
+++ etc/OthelloPlayer2.py
@@ -15,23 +15,44 @@ def Changable(i, j, di, dj, stone, board):
     return flag
 
 def GetPosition(stone, board):
-
+    # how aboout make proioty location lists
+    pro1 = ("0,0", "0,7", "7,0", "7,7")
+    pro2 = ("2,2", "5,2", "2,5", "5,5")
+    pro3 = ("0,2", "2,0", "5,0", "7,2", "0,5", "2,7", "5,7", "7,5")
+    pro4 = ("3,0", "4,0", "3,7", "4,7", "7,3", "7,4", "0,3", "0,4")
+    loc = []    
     for i in range(8):
         for j in range(8):
             if board[i][j] in 'WB':
                 continue
+            # if ocuppied, next
             for d in dirs:
                 di = d[0]
                 dj = d[1]
+            # check around blocks(8)
                 if stone == 'B':
                     enemy = 'W'
                 else:
                     enemy = 'B'
                 if 8 > i + di >= 0 and 8 > j + dj >= 0 and board[i + di][j + dj] == enemy and Changable(i, j, di, dj, stone, board):
-                    
                     loc += [str(i)+","+str(j)]
-                    return loc
-                    #break
+    #break
+    for k in loc:
+        if k in pro1:
+            return loc
+
+    for k in loc:
+        if k in pro2:
+            return loc
+        
+    for k in loc:
+        if k in pro3:
+            return loc
+        
+    for k in loc:
+        if k in pro4:
+            return loc
+    
     return "-1,-1"
 
 if __name__ == "__main__":
